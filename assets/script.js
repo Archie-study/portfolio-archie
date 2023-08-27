5ac1f0f74731224f21b4a3d8e211157f1d9750e0
archiestudy12@gmail.com <archiestudy12@gmail.com>
2023-08-27 20:30:25 +0700

    first commit

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav .nav-menu a')
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop-150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        
+        if (top > offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active')
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            })
+        }
+    })
+}
+
+
+
+// Hamburger Menu
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
